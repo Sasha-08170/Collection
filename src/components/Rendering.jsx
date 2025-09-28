f50af8df5443e8bf6aaf93270718b3b49838386f
JSDID <icegouse@gmail.com>
2025-09-28 21:01:16 +0300

    Add variable

--- src/components/Rendering.jsx
+++ src/components/Rendering.jsx
@@ -1,9 +1,10 @@
 const userName = "React";
+const userIcon = "🚀";
 
 const Reandering = () => {
   return <>
     <div className="page">
-      <h1 className="page__title">Hello {userName} 🚀</h1>
+      <h1 className="page__title">Hello {userName + userIcon} </h1>
     </div>
   </>
 }
