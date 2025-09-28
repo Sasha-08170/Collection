a293b0281de8a311c945ced9d71de9230efb0369
Sasha-08170 <102165823+Sasha-08170@users.noreply.github.com>
2025-09-28 20:48:33 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="page">
+      <h1 className="page__title">Hello React 🚀</h1>
+    </div>
+  );
+};
+
+export default App;
+
